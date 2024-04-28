3644df000047cf1da842b92d6687ed1c8bbf6da7
liviacnasc <liviac.nasc@gmail.com>
2024-04-28 18:35:55 -0300

    add: high_scored solution

--- high-scores/lib/high_scores.dart
+++ high-scores/lib/high_scores.dart
@@ -1,3 +1,24 @@
 class HighScores {
-  // Put your code here
+  late List<int> scores;
+  late List<int> sortedScore;
+
+  HighScores(this.scores){
+    this.sortedScore = List<int>.from(scores);
+    this.sortedScore.sort();
+  }
+
+  int latest(){
+    return scores.last;
+  }
+
+  List<int> personalTopThree(){
+    if(scores.length < 3){
+      return sortedScore.reversed.toList();
+    }
+    return List<int>.generate(3, (index) => sortedScore.reversed.elementAt(index));
+  }
+
+  int personalBest(){
+    return sortedScore.last;
+  }
 }

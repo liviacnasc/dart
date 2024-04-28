26ceab8fec2e356e2baf637ea6b381f1106be883
liviacnasc <liviac.nasc@gmail.com>
2024-04-28 13:38:02 -0300

    add: leap solution

--- leap/lib/leap.dart
+++ leap/lib/leap.dart
@@ -1,3 +1,11 @@
 class Leap {
-  
+  bool leapYear(int year){
+    if(year % 4 == 0){
+      if(year % 100 == 0){
+        return year % 400 == 0;
+      }
+      return true;
+    }
+    return false;
+  }
 }

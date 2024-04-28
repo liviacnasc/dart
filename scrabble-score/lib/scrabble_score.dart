1eed3d4261b1637ced9599739d1a5e182f561cfe
liviacnasc <liviac.nasc@gmail.com>
2024-04-28 16:08:18 -0300

    add: scrabble_score solution

--- scrabble-score/lib/scrabble_score.dart
+++ scrabble-score/lib/scrabble_score.dart
@@ -1,1 +1,24 @@
-c
+int score([String? word]){
+    if(word == null){
+      return 0;
+    }
+    int totalScore = 0;
+    Map<String, int> letters = {
+      "aeioulnrst": 1,
+      "dg": 2,
+      "bcmp": 3,
+      "fhvwy": 4,
+      "k": 5,
+      "jx": 8,
+      "qz": 10
+      };
+    word.toLowerCase().split('').forEach((element) {
+      for(var item in letters.entries){
+        if(item.key.contains(element)){
+          totalScore += item.value;
+          break;
+        }
+      }
+    });
+    return totalScore;
+}

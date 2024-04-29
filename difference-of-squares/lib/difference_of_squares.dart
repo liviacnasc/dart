2851a87141340e45ca6bbfe33f96b8ef23dce7b0
liviacnasc <liviac.nasc@gmail.com>
2024-04-28 22:43:54 -0300

    add: difference_of_squares solution

--- difference-of-squares/lib/difference_of_squares.dart
+++ difference-of-squares/lib/difference_of_squares.dart
@@ -1,3 +1,22 @@
+import 'dart:math';
+
 class DifferenceOfSquares {
-  // Put your code here
+
+  int differenceOfSquares(int number){
+    return squareOfSum(number) - sumOfSquares(number);
+  }
+  int sumOfSquares(int number){
+    int result = 0;
+    for(int i = 1; i <= number; i++){
+      result += pow(i, 2).toInt();
+    }
+    return result;
+  }
+  int squareOfSum(int number){
+    int result = 0;
+    for(int i = 1; i <= number; i++){
+      result += i;
+    }
+    return pow(result, 2).toInt();
+  }
 }
